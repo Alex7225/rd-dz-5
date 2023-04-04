5eacf9896b7b88c6aedf32e2adc147c3a6301c21
oleksandr <oleksandr_grigor@ukr.net>
2023-04-03 23:37:08 -0700

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a =7;
+        int b =7;
+        int c =5;
+        int d =10;
+
+        int sum1 = a+b;
+        int sum2 = c+d;
+
+        System.out.println(sum1>sum2); // 1-st compare
+        sum1++;
+        sum2=sum2-2;
+
+        System.out.println(sum1>sum2);   // 2-nd compare
+
+        System.out.println(sum1%2==0 || sum2%2==0); // divide & compare
+
+
     }
 }
